7e0c4ae60125ee18a7a79a8f2d679ad8d8a28638
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-03-27 17:03:50 -0700

    set up db abd MVT structure

---
+++ corgicollector/main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # defining the root route (path, view, name(optional, kwarg)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('corgis/', views.corgis_index, name='index'),
+]
--- corgicollector/main_app/views.py
+++ corgicollector/main_app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
+# This is temp for corgi data
+corgis = [
+    {'name': 'Clem', 'breed': 'corgi/aussie',
+        'description': 'furry little demon', 'age': 8},
+    {'name': 'Reese', 'breed': 'corgi/aussie',
+        'description': 'gentle and loving', 'age': 6},
+    {'name': 'Buddy', 'breed': 'Welsh Corgi Pembroke',
+        'description': 'Loves belly rubs and playing fetch', 'age': 5},
+    {'name': 'Luna', 'breed': 'Cardigan Welsh Corgi',
+        'description': 'Loves long walks and cuddling', 'age': 2},
+    {'name': 'Milo', 'breed': 'Corgi/Australian Shepherd mix',
+        'description': 'Energetic and playful', 'age': 1},
+]
+
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def corgis_index(request):
+    return render(request, 'corgis/index.html', {
+        'corgis': corgis
+    })
