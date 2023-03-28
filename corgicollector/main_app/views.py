42062af0866ab32ac100ae4ae96a905ae9d637db
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-03-28 16:42:35 -0700

    set up emmet for django html files, populated more corgi starter data in the shell

--- corgicollector/main_app/views.py
+++ corgicollector/main_app/views.py
@@ -1,26 +1,7 @@
 from django.shortcuts import render
 from .models import Corgi
 
-
-# This is temp for corgi data
-# corgis = [
-#     {'name': 'Clem', 'breed': 'corgi/aussie',
-#         'description': 'furry little demon', 'age': 8},
-#     {'name': 'Reese', 'breed': 'corgi/aussie',
-#         'description': 'gentle and loving', 'age': 6},
-#     {'name': 'Buddy', 'breed': 'Welsh Corgi Pembroke',
-#         'description': 'Loves belly rubs and playing fetch', 'age': 5},
-#     {'name': 'Luna', 'breed': 'Cardigan Welsh Corgi',
-#         'description': 'Loves long walks and cuddling', 'age': 2},
-#     {'name': 'Milo', 'breed': 'Corgi/Australian Shepherd mix',
-#         'description': 'Energetic and playful', 'age': 1},
-# ]
-
-# c = Corgi(name="Biscuit", breed='Sphinx', description='Evil looking cuddle monster. Hairless', age=2)
-# r = Corgi(name='Reese', breed='aussie/corgi',
-#           description='Sweet little guy, gentle and loving', age=6)
 # Create your views here.
-
 
 def home(request):
     return render(request, 'home.html')
